882bf10e8793479cbc52764b65c1cc2ba7d5a730
sovernut <orenomail4@gmail.com>
2021-05-01 12:38:23 +0700

    using http hooks to fetch tasks

--- realistic/src/components/NewTask/NewTask.js
+++ realistic/src/components/NewTask/NewTask.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
+import useHttp from '../../hooks/use-http'
 
 const NewTask = (props) => {
   const [isLoading, setIsLoading] = useState(false);
