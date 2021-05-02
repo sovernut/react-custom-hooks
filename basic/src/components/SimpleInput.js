7b2a57cb80228669071a58dbebcf857fd900066d
sovernut <orenomail4@gmail.com>
2021-05-02 20:38:43 +0700

    provide validate feedback

--- basic/src/components/SimpleInput.js
+++ basic/src/components/SimpleInput.js
@@ -2,6 +2,7 @@ import { useState, useRef } from "react";
 const SimpleInput = (props) => {
   const inputRef = useRef();
   const [enteredName, setenteredName] = useState(""); // use state you can reset value
+  const [enteredNameValid, setEnteredNameValid] = useState(true);
 
   const nameInputChangeHandler = (event) => {
     setenteredName(event.target.value);
@@ -9,16 +10,27 @@ const SimpleInput = (props) => {
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
+
+    if (enteredName.trim() == "") {
+      setEnteredNameValid(false);
+      return;
+    }
     console.log(enteredName);
-    setenteredName('') // here useState allow you to reset input 
+    setEnteredNameValid(true);
+    setenteredName(""); // here useState allow you to reset input
 
     // const enteredValue = inputRef.current.value;
     // inputRef.current.value = '' // this can reset value but not recommended!
     // console.log(enteredValue);
   };
+
+  const nameInputClasses = enteredNameValid
+    ? "form-control"
+    : "form-control invalid";
+
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
           ref={inputRef}
@@ -27,6 +39,7 @@ const SimpleInput = (props) => {
           onChange={nameInputChangeHandler}
           value={enteredName}
         />
+        {!enteredNameValid && <p className="error-text">Name can't be empty</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
