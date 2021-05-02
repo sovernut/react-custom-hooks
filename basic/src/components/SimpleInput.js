7e593e581cb8202ec7d6d00c453ede8cc1e4d889
sovernut <orenomail4@gmail.com>
2021-05-02 20:28:14 +0700

    ref vs useState

--- basic/src/components/SimpleInput.js
+++ basic/src/components/SimpleInput.js
@@ -1,9 +1,32 @@
+import { useState, useRef } from "react";
 const SimpleInput = (props) => {
+  const inputRef = useRef();
+  const [enteredName, setenteredName] = useState(""); // use state you can reset value
+
+  const nameInputChangeHandler = (event) => {
+    setenteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredName);
+    setenteredName('') // here useState allow you to reset input 
+
+    // const enteredValue = inputRef.current.value;
+    // inputRef.current.value = '' // this can reset value but not recommended!
+    // console.log(enteredValue);
+  };
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={inputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
